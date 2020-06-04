26f64957f7d0478ca9556b27bbb5c1ba30b2b273
Guilherme Laureano <guilhermeml001@gmail.com>
2020-06-04 00:44:20 -0300

    Create User

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import appointmentRoutes from './appointments.routes';
+import appointmentsRoutes from './appointments.routes';
+import usersRoutes from './users.routes';
 
 const routes = Router();
 
-routes.use('/appointments', appointmentRoutes);
+routes.use('/appointments', appointmentsRoutes);
+routes.use('/users', usersRoutes);
 
 export default routes;
---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,37 @@
+import { hash } from 'bcryptjs';
+import { getRepository } from 'typeorm';
+import User from '../models/User';
+
+interface Request {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  public async execute({ name, email, password }: Request): Promise<User> {
+    const userRepository = getRepository(User);
+
+    const checkUserExists = await userRepository.findOne({
+      where: { email },
+    });
+
+    if (checkUserExists) {
+      throw new Error('Email address already used.');
+    }
+
+    const hashedPasseord = await hash(password, 8);
+
+    const user = userRepository.create({
+      name,
+      email,
+      password: hashedPasseord,
+    });
+
+    await userRepository.save(user);
+
+    return user;
+  }
+}
+
+export default CreateUserService;
