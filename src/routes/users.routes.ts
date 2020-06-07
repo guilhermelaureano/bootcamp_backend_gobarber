29bf37abd717691150190c69937404e9a3211daf
Guilherme Laureano <guilhermeml001@gmail.com>
2020-06-07 15:11:12 -0300

    upload de avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import uploadConfig from '../config/upload';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 const usersRoute = Router();
+const upload = multer(uploadConfig);
 
 usersRoute.post('/', async (request, response) => {
   try {
@@ -23,5 +28,27 @@ usersRoute.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRoute.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      delete user.password;
+
+      return response.json(user);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
+  },
+);
 
 export default usersRoute;
